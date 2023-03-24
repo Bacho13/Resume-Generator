b088eb97c7999699e407de16b9b4a630b640738d
Bacho <bacholashkhia13@gmail.com>
2023-03-24 14:34:43 +0400

    tyinf fix error map in mainCOntent

--- pages/components/MainContent.js
+++ pages/components/MainContent.js
@@ -11,20 +11,21 @@ function MainContent({ eduOrExpo }) {
           <div className={styles.dotDecor}></div>
         </div>
 
-        {eduOrExpo.map((item) => (
-          <div className={styles.innerContent} key={item.id}>
-            <div className={styles.leftSide}>
-              <p className={styles.position}>{item.position}</p>
-              <p
-                className={styles.dates}
-              >{`${item.startDate} - ${item.endDate}`}</p>
+        {eduOrExpo &&
+          eduOrExpo.map((item) => (
+            <div className={styles.innerContent} key={item.id}>
+              <div className={styles.leftSide}>
+                <p className={styles.position}>{item.position}</p>
+                <p
+                  className={styles.dates}
+                >{`${item.startDate} - ${item.endDate}`}</p>
+              </div>
+              <div className={styles.rightSide}>
+                <p className={styles.companyName}>{item.company}</p>
+                <p className={styles.urJob}>{item.about}</p>
+              </div>
             </div>
-            <div className={styles.rightSide}>
-              <p className={styles.companyName}>{item.company}</p>
-              <p className={styles.urJob}>{item.about}</p>
-            </div>
-          </div>
-        ))}
+          ))}
       </div>
     </>
   );
